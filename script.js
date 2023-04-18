a48822f372eca44f589ec64a557f49677a835a60
limola23 <molanolili23@gmail.com>
2023-04-18 13:50:16 -0500

    basic login page

---
+++ script.js
@@ -0,0 +1,44 @@
+let bd = [
+    {
+        email:"mcocuy@mail.com",
+        password:"compaq",
+        username: "striker",
+        active: true
+    },
+
+    {
+        email:"CRocuy@mail.com",
+        password:"compaq2",
+        username:"carlosReact",
+        active:true
+    },
+
+    {
+        email:"DB@mail.com",
+        password:"compaq3",
+        username:"danielaButstrap",
+        active:false
+    }
+]
+
+function logIn() {
+    let username = document.querySelector("#username").value;
+    let password = document.querySelector("#password").value;
+    
+    for (let data of bd) {
+        if (data.username === username) {
+            if (data.password === password) {
+                alert("Inicio de sesión exitoso");
+                return;
+            } else {
+                alert("Datos de inicio de sesión incorrectos");
+                return;
+            }
+        }
+    }
+    
+    alert("Datos de inicio de sesión incorrectos");
+}
+
+logIn();
+
